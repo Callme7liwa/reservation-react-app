80dcd37a95d4b71c3d4911f4d6309aef03ea405d
Callme7liwa <ayoub.seddiki@usmba.ac.ma>
2023-06-22 18:59:20 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import {HomePage} from './Component/HomePage'
 import LoginPage from './Pages/Login/LoginPage';
+import WelcomPage from './Pages/Welcome/WelcomPage';
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <WelcomPage />
     </div>
   );
 }
---
+++ src/Pages/Welcome/WelcomPage.jsx
@@ -0,0 +1,41 @@
+import "./Welcome.css";
+
+const WelcomPage = () => {
+    return (
+        <div className="welcome-page-container">
+            <div className="navbar-container">
+                <div className="navbar-left">
+                    <h1>Study <span>Connect</span></h1>
+                </div>
+                <div className="navbar-center">
+                    <span>Welcome</span>
+                    <span>Reservation</span>
+                    <span>My Reservation</span>
+                </div>
+                <div className="navbar-right">
+                    <span>LogOut</span>
+                </div>
+            </div>
+
+            <div className="welcome-body">
+                <div className="welcome-body-left">
+                    <div className="welcoming-title">
+                        <h1> Welcome There  </h1>
+                        <h1 className="name"> Derkaoui Hatim !</h1>
+                    </div>
+                    <div className="line">
+                        <div className="line-demi"></div>
+                        <div className="circle"></div>
+                        <div className="line-demi"></div>
+                    </div>
+                  
+                </div>
+                <div className="welcome-body-right">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WelcomPage;
